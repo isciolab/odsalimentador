32bb1a435a78659ff22cf675b73748464dc79998
fernando <fernandoga22@gmail.com>
2018-08-23 08:22:58 -0500

    correccion migrate

--- db/migrate/20180822154332_update_colum_date_goal_cities.rb
+++ db/migrate/20180822154332_update_colum_date_goal_cities.rb
@@ -1,5 +1,8 @@
 class UpdateColumDateGoalCities < ActiveRecord::Migration[5.1]
-  def change
+  def up
     rename_column :goal_cities, :date, :date_from
+  end
+  def down
+    rename_column :goal_cities, :date_from, :date
   end
 end

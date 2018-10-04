03ed58a7b422358ed311de1d6c5f4b25cedf04b1
Jhainey <milevisj@gmail.com>
2018-10-04 16:07:03 -0500

    Merge branch 'master' of https://bitbucket.org/equipodedesarrollodisegna/odsalimentador
    Conflicts:
    	db/seeds.rb

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,14 @@ module Odsalimenta
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # 
+    #
+    #
+    use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end
+    end
   end
 end

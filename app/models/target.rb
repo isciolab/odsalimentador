55b492f4ec80184b4347a4d37cf50c0e7ccb3851
fernando <fernandoga22@gmail.com>
2018-10-06 17:28:25 -0500

    correccion

--- app/models/target.rb
+++ app/models/target.rb
@@ -5,6 +5,7 @@ class Target < ApplicationRecord
   belongs_to :goal, ->{ joins(:targets) }
   has_many  :indicators
 
+  ##este metodo concatena el numero de la meta con el nombre, para ser invocado mas que todo en los select de metas
   def numbername
     self.number + " - " + self.name
   end

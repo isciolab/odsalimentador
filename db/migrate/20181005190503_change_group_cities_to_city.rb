af125d4bad27bae92fbcf0ae717b4a30133d6edc
fernando <fernandoga22@gmail.com>
2018-10-08 19:01:08 -0500

    correccion

---
+++ db/migrate/20181005190503_change_group_cities_to_city.rb
@@ -0,0 +1,12 @@
+class ChangeGroupCitiesToCity < ActiveRecord::Migration[5.1]
+   def up
+       execute "ALTER TABLE `cities`
+ CHANGE `group_cities_id` `group_city_id` BIGINT(20) NULL;
+   "
+     end
+   def down
+       execute "ALTER TABLE `cities`
+ CHANGE `group_city_id` `group_cities_id` BIGINT(20) NULL;
+   "
+     end
+  end

34d211de9bd778c98ae890991e3dd196ef6a60ac
fernando <fernandoga22@gmail.com>
2018-10-18 16:36:50 -0500

    api city

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -1,6 +1,6 @@
 class Api::CitiesController < ApplicationController
   def getCities
-    @cities= City.where(delete_at: [nil])
+    @cities= City.where(delete_at: [nil]).to_json(include: :department)
     render json: @cities, status: :ok
   end
 
